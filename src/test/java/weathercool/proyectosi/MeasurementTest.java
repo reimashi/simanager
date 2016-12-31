169336bf6e120841a2559735966f6f79332dedd3
srfernandez <srfernandez93@gmail.com>
2016-12-31 01:51:11 +0100

    Añadidos mas test de measures

--- src/test/java/weathercool/proyectosi/MeasurementTest.java
+++ src/test/java/weathercool/proyectosi/MeasurementTest.java
@@ -1,7 +1,9 @@
 package weathercool.proyectosi;
 
 import static org.junit.Assert.*;
+import static weathercool.proyectosi.TransactionUtils.doTransaction;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -10,9 +12,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static weathercool.proyectosi.TransactionUtils.doTransaction;
 
 public class MeasurementTest extends SQLBasedTest {
 	private static EntityManagerFactory emf;
@@ -27,13 +33,59 @@ public class MeasurementTest extends SQLBasedTest {
 		if(emf!=null && emf.isOpen()) emf.close();
 	}
 	
+	@After
+	public void renewConnectionAfterTest() throws ClassNotFoundException, SQLException {
+        Statement statement = jdbcConnection.createStatement();
+        statement.execute("DELETE FROM alert");
+
+		super.renewConnection();
+	}
+	
+	@Test
+	public void testCreateMeasurement() throws SQLException {
+
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO location(latitude, longitude) values(94.132, 45.189)", 
+				Statement.RETURN_GENERATED_KEYS);
+		int locId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO alert(temperature_high, temperature_half, temperature_low, rain_high, rain_half, rain_low) values(15, 25, 27, 30, 1, 23)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int alertId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO time(year, month, day, hour, minute, second) values(2016, 11, 20, 17, 55, 38)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+		int timeId = getLastInsertedId(statement);
+		
+		statement.executeUpdate(
+				"INSERT INTO Measurement(id_alert, id_location, id_time, elevation, temperature_surface, temperature_500mb,"
+				+ "temperature_850mb, temperature_sea_level, cloud_cover_high, cloud_cover_half, cloud_cover_low,"
+				+ "visibility, salinity, water_speed_eastward, water_speed_northward, wave_direction_mean,wave_period_absolute, wave_period_peak, wave_direction_peak,"
+				+ "snow_level, snow_precipitation, rain_pecipitation, humidity, wind_direction, wind_lon, wind_lat, wind_gust) "
+				+ "values("+alertId+","+locId+","+timeId+",0,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)", 
+				Statement.RETURN_GENERATED_KEYS);
+		// check
+		Statement statement2 = jdbcConnection.createStatement();
+		ResultSet rs = statement2.executeQuery("SELECT COUNT(*) AS Total FROM Measurement WHERE alert_id = "+ alertId);
+		rs.next();
+
+		assertEquals(1, rs.getInt("total"));
+	}
+	
 	@Test
 	public void testFindMeasures() throws SQLException {
 		Statement statement = jdbcConnection.createStatement();
-		int locId = statement.executeUpdate(
+		statement.executeUpdate(
 				"INSERT INTO location(latitude, longitude) values(94.132, 45.189)", 
 				Statement.RETURN_GENERATED_KEYS);
-		
+		int locId = getLastInsertedId(statement);
 		statement = jdbcConnection.createStatement();
 		statement.executeUpdate(
 				"INSERT INTO alert(temperature_high, temperature_half, temperature_low, rain_high, rain_half, rain_low) values(15, 25, 27, 30, 1, 23)",
@@ -50,7 +102,7 @@ public class MeasurementTest extends SQLBasedTest {
 		
 		statement = jdbcConnection.createStatement();
 		statement.executeUpdate(
-				"INSERT INTO Measurement(alert_id, location_id, time_id, elevation, temperature_surface, temperature_500mb,"
+				"INSERT INTO Measurement(id_alert, id_location, id_time, elevation, temperature_surface, temperature_500mb,"
 				+ "temperature_850mb, temperature_sea_level, cloud_cover_high, cloud_cover_half, cloud_cover_low,"
 				+ "visibility, salinity, water_speed_eastward, water_speed_northward, wave_direction_mean,wave_period_absolute, wave_period_peak, wave_direction_peak,"
 				+ "snow_level, snow_precipitation, rain_pecipitation, humidity, wind_direction, wind_lon, wind_lat, wind_gust) "
@@ -68,5 +120,114 @@ public class MeasurementTest extends SQLBasedTest {
 		//check
 		assertEquals(1, measures.size());
 		assertEquals(alertId, measures.get(0).getAlert());
+	}
+	
+	@Test
+	public void testDeleteMeasure() throws SQLException {
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO location(latitude, longitude) values(94.132, 45.189)", 
+				Statement.RETURN_GENERATED_KEYS);
+		int locId = getLastInsertedId(statement);
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO alert(temperature_high, temperature_half, temperature_low, rain_high, rain_half, rain_low) values(15, 25, 27, 30, 1, 23)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int alertId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO time(year, month, day, hour, minute, second) values(2016, 11, 20, 17, 55, 38)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+		int timeId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO Measurement(id_alert, id_location, id_time, elevation, temperature_surface, temperature_500mb,"
+				+ "temperature_850mb, temperature_sea_level, cloud_cover_high, cloud_cover_half, cloud_cover_low,"
+				+ "visibility, salinity, water_speed_eastward, water_speed_northward, wave_direction_mean,wave_period_absolute, wave_period_peak, wave_direction_peak,"
+				+ "snow_level, snow_precipitation, rain_pecipitation, humidity, wind_direction, wind_lon, wind_lat, wind_gust) "
+				+ "values("+alertId+","+locId+","+timeId+",0,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)", 
+				Statement.RETURN_GENERATED_KEYS);
+		int id = getLastInsertedId(statement);
+
+		doTransaction(emf, me -> {
+			Measurement m = me.find(Measurement.class, id);
+			me.remove(m);
+		});
+
+		// check
+		statement = jdbcConnection.createStatement();
+		ResultSet rs = statement.executeQuery("SELECT COUNT(*) as total FROM Measurement WHERE id = " + id);
+		rs.next();
+
+		assertEquals(0, rs.getInt("total"));
+	}
+	
+	@Test
+	public void testFindById() throws SQLException {
+		// prepare database for test
+		Statement statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO location(latitude, longitude) values(94.132, 45.189)", 
+				Statement.RETURN_GENERATED_KEYS);
+		int locId = getLastInsertedId(statement);
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO alert(temperature_high, temperature_half, temperature_low, rain_high, rain_half, rain_low) values(15, 25, 27, 30, 1, 23)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		int alertId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO time(year, month, day, hour, minute, second) values(2016, 11, 20, 17, 55, 38)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+		int timeId = getLastInsertedId(statement);
+		
+		statement = jdbcConnection.createStatement();
+		statement.executeUpdate(
+				"INSERT INTO Measurement(id_alert, id_location, id_time, elevation, temperature_surface, temperature_500mb,"
+				+ "temperature_850mb, temperature_sea_level, cloud_cover_high, cloud_cover_half, cloud_cover_low,"
+				+ "visibility, salinity, water_speed_eastward, water_speed_northward, wave_direction_mean,wave_period_absolute, wave_period_peak, wave_direction_peak,"
+				+ "snow_level, snow_precipitation, rain_pecipitation, humidity, wind_direction, wind_lon, wind_lat, wind_gust) "
+				+ "values("+alertId+","+locId+","+timeId+",0,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+		int id = getLastInsertedId(statement);
+
+		// test code
+		EntityManager em = emf.createEntityManager();
+		Measurement me = em.find(Measurement.class, id);
+
+		// assert code
+		assertEquals(0, me.getElevation(), 1e-10);
+		assertEquals(20, me.getTemperatureSurface(), 1e-10);
+		assertEquals(0, me.getTemperature500mb(), 1e-10);
+		assertEquals(0, me.getTemperature850mb(), 1e-10);
+		assertEquals(0, me.getTemperatureSeaLevel(), 1e-10);
+		assertEquals(0, me.getCloudCoverHigh(), 1e-10);
+		assertEquals(0, me.getCloudCoverHalf(), 1e-10);
+		assertEquals(0, me.getCloudCoverLow(), 1e-10);
+		assertEquals(0, me.getVisibility(), 1e-10);
+		assertEquals(0, me.getSalinity(), 1e-10);
+		assertEquals(0, me.getWaterSpeedEastward(), 1e-10);
+		assertEquals(0, me.getWaterSpeedNorthward(), 1e-10);
+		assertEquals(0, me.getWaterDirectionMean(), 1e-10);
+		assertEquals(0, me.getWaterDirectionPeak(), 1e-10);
+		assertEquals(0, me.getWaterPeriodAbsolute(), 1e-10);
+		assertEquals(0, me.getWaterPeriodPeak(), 1e-10);
+		assertEquals(0, me.getSnowLevel(), 1e-10);
+		assertEquals(0, me.getSnowPrecipitation(), 1e-10);
+		assertEquals(0, me.getRainPrecipitation(), 1e-10);
+		assertEquals(0, me.getHumidity(), 1e-10);
+		assertEquals(0, me.getWindDirection(), 1e-10);
+		assertEquals(0, me.getWindLon(), 1e-10);
+		assertEquals(0, me.getWindLat(), 1e-10);
+		assertEquals(0, me.getWindGust(), 1e-10);
+		
 	}
 }
